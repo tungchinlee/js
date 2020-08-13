9e002aab5825d9762f432365d4e6510e8b372718
tungchinlee <tungchinli@gmail.com>
2020-08-13 21:23:31 +0800

    tai

--- hello.js
+++ hello.js
@@ -46,4 +46,22 @@
 //   console.log(`i made dinner with #{i1}`)
 // }
 
+// let array = [1, 2, 3, 4]
+//  array[9] = 6;
+//  console.log(array[5])
 
+ let all_songlists = []
+
+let musicPlayList = {
+  owner: {account: 'user1', nickname: 'lee', isVip: true},
+  playlist_name: 'lovesong',
+  playlist_duration: 3000, //sum of song lengths
+  
+  songs: [
+      {name: '喔', genre: 'string', length: 60, artist: '板橋金城武', album: '', isFavorited: true, playedＣount: 3},
+      {name: '人生海海', genre: 'string', length: 60, artist: '炯和 AKA 千億', album: '', isFavorited: false, playedCount: 2 }
+  ]}
+
+  all_songlists.push(musicPlayList)
+console.log(all_songlists)
+console.log(musicPlayList.songs[1])
--- pastleo-js-lesson-master/05_timer-jquery/DOMapi.js
+++ pastleo-js-lesson-master/05_timer-jquery/DOMapi.js
@@ -1,4 +1,4 @@
-window.addEventListener('DOMContextLoaded', function(){
+window.addEventListener('DOMContentLoaded', function(){
 
   let cnt = 0, running = false, interval;
   
@@ -7,16 +7,16 @@ window.addEventListener('DOMContextLoaded', function(){
       document.querySelector('button').textContent = 'start'
       clearInterval(interval)
       running = false
-      document.getElementById('running').slideUp()
+      document.getElementById('running').style.cssText = "display: none;"
     }else{
       interval = setInterval(function(){
         cnt = cnt + 1;
         document.getElementById('seconds').textContent = ( Math.floor ( cnt / 10 ) )
-        document.querySelector('.dots div:last-child').prependTo('.dots')
+        document.querySelector('.dots div:last-child').prepend(div)
       }, 100)
       document.querySelector('button').textContent = 'stop'
       running = true
-      document.getElementById('running').slideDown()
+      document.getElementById('running').style.cssText = "display: block;"
     }
   })
 })
